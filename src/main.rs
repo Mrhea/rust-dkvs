166841d07aaa4a9f4a22fce3fe17add19e941f38
Matthew Rhea <m.b.rhea31@gmail.com>
2020-07-22 21:22:43 -0700

    fix typo

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use actix-web::{web, App, HttpResponse, HttpServer, Responder};
 
 async fn getHello() -> impl Responder {
     HttpResponse::Ok().body("Hello, world!")
