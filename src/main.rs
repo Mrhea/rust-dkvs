761d6e816f836198178dee44f0c4c015284ec4bc
Matthew Rhea <m.b.rhea31@gmail.com>
2020-07-22 21:19:20 -0700

    Switched to actix-web for async

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,29 @@
-#![feature(proc_macro_hygiene, decl_macro)]
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 
-#[macro_use] extern crate rocket;
-
-#[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+async fn getHello() -> impl Responder {
+    HttpResponse::Ok().body("Hello, world!")
 }
 
-fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+async fn getTest() -> impl Responder {
+    HttpResponse::Ok().body("GET request received.")
+} 
+
+// async fn postHello() -> impl Responder {
+
+// }
+
+// async fn postTest() -> impl Responder {
+
+// }
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/getHello", web::get().to(getHello))
+            .route("/getTest" ,  web::get().to(getTest))
+    })
+    .bind("127.0.0.1:8088")?
+    .run()
+    .await
 }
