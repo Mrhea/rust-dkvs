8038b190338735c75d3afa5867fd75ce0ead8a26
Matthew Rhea <m.b.rhea31@gmail.com>
2020-07-22 21:24:50 -0700

    Gotta remove async to update

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,29 @@
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+// use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 
-async fn getHello() -> impl Responder {
-    HttpResponse::Ok().body("Hello, world!")
-}
-
-async fn getTest() -> impl Responder {
-    HttpResponse::Ok().body("GET request received.")
-} 
-
-// async fn postHello() -> impl Responder {
-
+// fn getHello() -> impl Responder {
+//     HttpResponse::Ok().body("Hello, world!")
 // }
 
-// async fn postTest() -> impl Responder {
+// fn getTest() -> impl Responder {
+//     HttpResponse::Ok().body("GET request received.")
+// } 
 
+// // async fn postHello() -> impl Responder {
+
+// // }
+
+// // async fn postTest() -> impl Responder {
+
+// // }
+
+// #[actix_rt::main]
+// fn main() -> std::io::Result<()> {
+//     HttpServer::new(|| {
+//         App::new()
+//             .route("/getHello", web::get().to(getHello))
+//             .route("/getTest" ,  web::get().to(getTest))
+//     })
+//     .bind("127.0.0.1:8088")?
+//     .run()
+//     .await
 // }
-
-#[actix_rt::main]
-async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .route("/getHello", web::get().to(getHello))
-            .route("/getTest" ,  web::get().to(getTest))
-    })
-    .bind("127.0.0.1:8088")?
-    .run()
-    .await
-}
