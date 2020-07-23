14274ff50e97b0c49bc5b2f98137638b8fe644d6
Matthew Rhea <m.b.rhea31@gmail.com>
2020-07-22 22:23:36 -0700

    Asgn 1

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,39 @@
-// use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 
-// fn getHello() -> impl Responder {
-//     HttpResponse::Ok().body("Hello, world!")
-// }
+async fn index() -> impl Responder {
+    HttpResponse::Ok().body("This is the index.")
+}
 
-// fn getTest() -> impl Responder {
-//     HttpResponse::Ok().body("GET request received.")
-// } 
+async fn get_hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello, world!")
+}
 
-// // async fn postHello() -> impl Responder {
+async fn get_test() -> impl Responder {
+    HttpResponse::Ok().body("GET request received.")
+} 
 
-// // }
+async fn post_test(bytes: web::Bytes) -> Result<String, HttpResponse> {
+    match String::from_utf8(bytes.to_vec()) {
+        Ok(text) => Ok(format!("Hello, {}!\n", text)),
+        Err(_) => Err(HttpResponse::BadRequest().into())
+    }
+}
 
-// // async fn postTest() -> impl Responder {
+async fn post_hello() -> impl Responder {
+    HttpResponse::BadRequest()
+}
 
-// // }
-
-// #[actix_rt::main]
-// fn main() -> std::io::Result<()> {
-//     HttpServer::new(|| {
-//         App::new()
-//             .route("/getHello", web::get().to(getHello))
-//             .route("/getTest" ,  web::get().to(getTest))
-//     })
-//     .bind("127.0.0.1:8088")?
-//     .run()
-//     .await
-// }
-fn main() {
-    println!("Hello again");
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/"   ,  web::get().to(index))
+            .route("/getHello", web::get().to(get_hello))
+            .route("/getTest" , web::get().to(get_test))
+            .route("/postHello", web::post().to(post_hello))
+            .route("/postTest" , web::post().to(post_test))
+    })
+    .bind("127.0.0.1:8088")?
+    .run()
+    .await
 }
